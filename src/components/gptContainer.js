ef324b582f65dceb69b7b4a20af50a13733df611
王家敏 <mggis0or1@gmail.com>
2024-01-15 13:11:27 +0800

    update css

--- src/components/gptContainer.js
+++ src/components/gptContainer.js
@@ -2,28 +2,31 @@ import React from 'react';
 
 const GPTCard = ({ title, imageUrl, description, linkUrl }) => {
     return (
-        <a
-            href={linkUrl}
-            className="group flex flex-col items-center justify-center rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <h2 className="mb-3 text-2xl font-semibold">
-                {title.replace("ChatGPT - ", "")}{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            </span>
-            </h2>
-            <img
-                src={imageUrl}
-                alt={title}
-                className="w-48 h-48 object-cover py-4 items-center"
-            />
-            <p className="m-0 max-w-[30ch] text-sm opacity-50 items-center">
-                {description}
-            </p>
-        </a>
+        <div className="py-2 px-4">
+            <a
+                href={linkUrl}
+                className="group flex flex-col items-center justify-center rounded-2xl shadow-lg transform transition duration-500 hover:scale-110 hover:shadow-2xl bg-gradient-to-r from-blue-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 p-6"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <h2 className="mb-4 text-xl md:text-2xl font-bold text-white truncate">
+                    {title.replace("ChatGPT - ", "")}
+                </h2>
+                <div className="w-36 h-36 lg:w-48 lg:h-48 mb-4">
+                    <img
+                        src={imageUrl}
+                        alt={title}
+                        className="w-full h-full object-cover rounded-full border-4 border-white shadow"
+                    />
+                </div>
+                <p className="text-center text-sm md:text-base text-white max-w-md">
+                    {description}
+                </p>
+            </a>
+        </div>
     );
-
 };
+
+
 
 export default GPTCard;
