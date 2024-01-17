be06476f0f41c7b0dbad274af074d2a30b654b75
王家敏 <mggis0or1@gmail.com>
2024-01-17 15:58:01 +0800

    add earth

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home({gptDataArray}) {
                 <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
                     <div
-                        className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                        className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none flex">
                         <a
                             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
                             href="https://mggg.cloud"
@@ -25,14 +25,102 @@ export default function Home({gptDataArray}) {
                         >
                             <p className="text-xl text-black">Created By mggg.cloud </p>
                         </a>
+
+
+                    </div>
+                </div>
+
+                <div className="section-banner">
+                    <div id="star-1">
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-bottomright"></div>
+                            <div id="curved-corner-bottomleft"></div>
+                        </div>
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-topright"></div>
+                            <div id="curved-corner-topleft"></div>
+                        </div>
+                    </div>
+
+                    <div id="star-2">
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-bottomright"></div>
+                            <div id="curved-corner-bottomleft"></div>
+                        </div>
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-topright"></div>
+                            <div id="curved-corner-topleft"></div>
+                        </div>
+                    </div>
+
+                    <div id="star-3">
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-bottomright"></div>
+                            <div id="curved-corner-bottomleft"></div>
+                        </div>
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-topright"></div>
+                            <div id="curved-corner-topleft"></div>
+                        </div>
+                    </div>
+
+                    <div id="star-4">
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-bottomright"></div>
+                            <div id="curved-corner-bottomleft"></div>
+                        </div>
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-topright"></div>
+                            <div id="curved-corner-topleft"></div>
+                        </div>
+                    </div>
+
+                    <div id="star-5">
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-bottomright"></div>
+                            <div id="curved-corner-bottomleft"></div>
+                        </div>
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-topright"></div>
+                            <div id="curved-corner-topleft"></div>
+                        </div>
+                    </div>
+
+                    <div id="star-6">
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-bottomright"></div>
+                            <div id="curved-corner-bottomleft"></div>
+                        </div>
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-topright"></div>
+                            <div id="curved-corner-topleft"></div>
+                        </div>
+                    </div>
+
+                    <div id="star-7">
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-bottomright"></div>
+                            <div id="curved-corner-bottomleft"></div>
+                        </div>
+                        <div className="curved-corner-star">
+                            <div id="curved-corner-topright"></div>
+                            <div id="curved-corner-topleft"></div>
+                        </div>
                     </div>
                 </div>
 
                 <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] py-24">
-                    <div className="spinner">
-                        <div className="spinnerin"></div>
-                    </div>
-                    <h1 className="ml-6 text-6xl text-black px-2">Mggg GPTS Gallery</h1>
+                    <button className="btn" type="button">
+                        <h1> <strong>Mggg GPTS Gallery</strong> </h1>
+                        <div id="container-stars">
+                            <div id="stars"></div>
+                        </div>
+
+                        <div id="glow">
+                            <div className="circle"></div>
+                            <div className="circle"></div>
+                        </div>
+                    </button>
 
                 </div>
 
